465661c64dcee0728666a880b34f5f9a66a1402c
Michael Bayne <mdb@samskivert.com>
2010-09-06 16:02:44 -0700

    Start up the HTTP server and wire up a signal handler to cleanly terminate on receipt of ctrl-c.

--- environ/src/main/scala/coreen/server/Main.scala
+++ environ/src/main/scala/coreen/server/Main.scala
@@ -3,6 +3,8 @@
 
 package coreen.server
 
+import com.samskivert.util.SignalUtil
+
 /**
  * The main entry point for the Coreen server.
  */
@@ -11,7 +13,23 @@ object Main
   val log = com.samskivert.util.Logger.getLogger("coreen")
 
   def main (args :Array[String]) {
+    // initialize our various components
     val httpServer = new HttpServer
     httpServer.init
+    httpServer.start
+
+    // register a signal handler to shutdown gracefully on ctrl-c
+    val pause = new AnyRef
+    SignalUtil.register(SignalUtil.Number.INT, new SignalUtil.Handler {
+      def signalReceived (num :SignalUtil.Number) {
+        log.info("Coreen server exiting...")
+        httpServer.shutdown
+        pause.synchronized { pause.notify }
+      }
+    })
+    log.info("Coreen server running. Ctrl-c to exit.")
+
+    // block the main thread until our signal is received
+    pause.synchronized { pause.wait }
   }
 }
