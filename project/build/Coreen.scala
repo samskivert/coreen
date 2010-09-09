908c3f0ba5d0dce3c47aebaddc7ed48740909a87
Michael Bayne <mdb@baltic.(none)>
2010-09-09 10:40:47 -0700

    We're getting samskivert and gwt-utils from Maven now. Presently we need to get them from our local repository, so I've added that, but shortly they'll be synced to Maven Central, at which point that too can go away.

--- project/build/Coreen.scala
+++ project/build/Coreen.scala
@@ -3,13 +3,10 @@ import sbt._
 
 class Coreen (info :ProjectInfo) extends ParentProject(info) {
   lazy val util = project("util", "Util", new DefaultProject(_) {
+    val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
     val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
     val scalaj_collection = "org.scalaj" %% "scalaj-collection" % "1.0"
-
-    val ivyPattern = "[organization]/[module]/[revision]/[type]s/[artifact].[ext]"
-    val samskivertRepo = Resolver.url("samskivert").artifacts(
-      "http://samskivert.googlecode.com/svn/releases/" + ivyPattern)
-    val samskivert = "com.samskivert" % "samskivert" % "1.0"
+    val gwtUtils = "com.threerings" % "gwt-utils" % "1.0"
   })
 
   lazy val environ = project("environ", "Environment", new DefaultProject(_) {
