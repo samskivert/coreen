0ec394c9cbdc3a884693272ebc208aaabc468fc0
Michael Bayne <mdb@samskivert.com>
2010-09-10 12:29:41 -0700

    Rearranged dependencies. Switched to more concise but runs-all-the-time form for the i18nsync and genasync actions.

--- project/build/Coreen.scala
+++ project/build/Coreen.scala
@@ -3,46 +3,28 @@ import sbt._
 
 class Coreen (info :ProjectInfo) extends ParentProject(info) {
   lazy val util = project("util", "Util", new DefaultProject(_) {
-    val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
     val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
     val scalaj_collection = "org.scalaj" %% "scalaj-collection" % "1.0"
-    val gwtUtils = "com.threerings" % "gwt-utils" % "1.0-SNAPSHOT"
   })
 
   lazy val environ = project("environ", "Environment", new DefaultProject(_) {
-    // we need GWT user and dev for compiling the GWT bits and for devmode
-    val gwtUser = "com.google.gwt" % "gwt-user" % "2.0.4"
-    val gwtDev = "com.google.gwt" % "gwt-dev" % "2.0.4"
+    // TEMP: until our bits are uploaded to Maven Central
+    val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
 
+    val gwtDev = "com.google.gwt" % "gwt-dev" % "2.0.4"
+    val gwtUser = "com.google.gwt" % "gwt-user" % "2.0.4"
+    val gwtUtils = "com.threerings" % "gwt-utils" % "1.0-SNAPSHOT"
     val jetty = "org.mortbay.jetty" % "jetty" % "6.1.25"
 
-    // add a custom Ivy repository for gwt-asyncgen
-    val ivyPattern = "[organization]/[module]/[revision]/[type]s/[artifact].[ext]"
-    val gwtAsyncGenRepo = Resolver.url("gwt-asyncgen").artifacts(
-      "http://gwt-asyncgen.googlecode.com/svn/releases/" + ivyPattern)
-    val gwtAsyncGen = "com.samskivert" % "gwt-asyncgen" % "1.0"
-
     // generates FooServiceAsync classes from FooService classes for GWT RPC
-    def gentask (svcs :PathFinder) = {
-      val sources = svcs.getRelativePaths.map(_.replaceAll("Service.java", "ServiceAsync.java"))
-      val sourcePaths = sources.map(f => Path.fromString(info.projectPath, f))
-      fileTask(sourcePaths from svcs) {
-        defaultRunner.run("com.samskivert.asyncgen.AsyncGenTool", compileClasspath.get,
-                          svcs.getPaths.toList, log)
-      }
-    }
-    lazy val genasync = gentask(mainJavaSourcePath ** "*Service.java")
+    val gwtAsyncGen = "com.samskivert" % "gwt-asyncgen" % "1.0"
+    lazy val genasync = runTask(Some("com.samskivert.asyncgen.AsyncGenTool"), compileClasspath,
+                                (mainJavaSourcePath ** "*Service.java" getPaths).toList)
 
     // generates FooMessages.java from FooMessages.properties for GWT i18n
-    def i18ntask (props :PathFinder) = {
-      val sources = props.getRelativePaths.map(_.replaceAll(".properties$", ".java"))
-      val sourcePaths = sources.map(f => Path.fromString(info.projectPath, f))
-      fileTask(sourcePaths from props) {
-        defaultRunner.run("com.threerings.gwt.tools.I18nSync", compileClasspath.get,
-                          mainJavaSourcePath.absolutePath :: props.getPaths.toList, log)
-      }
-    }
-    lazy val i18nsync = i18ntask(mainJavaSourcePath ** "*Messages.properties")
+    lazy val i18nsync = runTask(Some("com.threerings.gwt.tools.I18nSync"), compileClasspath,
+                                mainJavaSourcePath.absolutePath :: (
+                                  mainJavaSourcePath ** "*Messages.properties" getPaths).toList)
 
     override def compileAction = super.compileAction dependsOn(i18nsync) dependsOn(genasync)
   }, util, javaReader)
