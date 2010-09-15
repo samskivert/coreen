9563bd6cd2d81fc6e4a00a730c447a8a1744e3d4
Michael Bayne <mdb@samskivert.com>
2010-09-14 18:12:12 -0700

    Beginnings of project identification.

--- environ/src/main/scala/coreen/ingress/Importer.scala
+++ environ/src/main/scala/coreen/ingress/Importer.scala
@@ -3,7 +3,10 @@
 
 package coreen.ingress
 
+import java.io.File
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.Executors
+import java.util.regex.Pattern
 
 import scala.collection.JavaConversions._
 
@@ -16,44 +19,63 @@ import coreen.server.Main.log
 object Importer
 {
   /** Requests a snapshot of the currently pending projects. */
-  def getPendingProjects :Array[PendingProject] = _projects.values map(_._1) toArray
+  def getPendingProjects :Array[PendingProject] = _projects.values.toArray sortBy(_.started)
 
   /** Requests that the project at the specified source be imported. */
   def importProject (source :String) :PendingProject = {
     val now = System.currentTimeMillis
     val pp = new PendingProject(source, "Starting...", now, now, false)
-    _projects += (source -> (pp, new Importer(source)))
+    _projects += (source -> pp)
+    _executor.execute(new Runnable {
+      override def run = processImport(source)
+    })
     pp
   }
 
-  private def updateProject (source :String, status :String) {
+  private def updateProject (source :String, status :String, complete :Boolean) {
     _projects.get(source) match {
-      case (pp, _) => {
+      case Some(pp) => {
         // this instance may be going out over the wire as this update happens, but even in the
         // event of such a race, nothing especially bad is going to happen
         pp.status = status
         pp.lastUpdated = System.currentTimeMillis
+        pp.complete = complete
       }
-      case _ => {
+      case None => {
         log.warning("Requested to update unknown project", "source", source, "status", status);
       }
     }
   }
 
-  private val _projects = new ConcurrentHashMap[String,(PendingProject,Importer)]()
-}
+  // methods from here down are invoked on a separate worker thread; be careful!
+  private def processImport (source :String) {
+    updateProject(source, "Identifying source...", false)
 
-private class Importer (source :String) extends Thread
-{
-  import Importer._
-
-  /* ctor */ {
-    start
+    // see if we can figure out where the data is
+    val file = new File(source)
+    if (file.isDirectory) {
+      localProjectImport(source, file)
+    } else if (file.exists && ARCHIVE_SUFFS.matcher(file.getName()).matches) {
+      localArchiveImport(source, file)
+    // } else if (GIT_URL.matches(sourcE)) {
+    // } else if (SVN_URL.matches(sourcE)) {
+    // } else if (HG_URL.matches(sourcE)) {
+    } else {
+      updateProject(source, "Unable to identify source", true)
+    }
   }
 
-  override def run {
-    updateProject(source, "Identifying source...")
-
-    // TODO: see if the source looks like a URL, or a directory, or a file on the filesystem, etc.
+  def localProjectImport (source :String, file :File) {
+    updateProject(source, "TODO: local project import...", false)
   }
+
+  def localArchiveImport (source :String, file :File) {
+    updateProject(source, "TODO: local archive import...", false)
+  }
+
+  private val _projects :collection.mutable.Map[String,PendingProject] =
+    new ConcurrentHashMap[String,PendingProject]()
+  private val _executor = Executors.newFixedThreadPool(4) // TODO: configurable
+
+  private val ARCHIVE_SUFFS = Pattern.compile("(.tgz|.tar.gz|.zip|.jar)$")
 }
