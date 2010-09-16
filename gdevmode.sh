df4550775c2e538130347903be556a0ad4b004f9
Michael Bayne <mdb@samskivert.com>
2010-09-16 15:31:44 -0700

    Moved java-reader into a totally separate project. Moved everything in environ to the top-level. I'd rather have the name-resolvers live in separate projects so that all languages are on equal footing.

--- gdevmode.sh
+++ gdevmode.sh
@@ -3,14 +3,14 @@
 GWT_UTILS_HOME=$HOME/projects/gwt-utils
 
 CLASSPATH=`echo gdevmode/lib_managed/scala_2.8.0/compile/gwt-dev-*.jar \
-    environ/lib_managed/scala_2.8.0/compile/gwt-user-*.jar \
-    environ/lib_managed/scala_2.8.0/compile/samskivert-*.jar \
+    lib_managed/scala_2.8.0/compile/gwt-user-*.jar \
+    lib_managed/scala_2.8.0/compile/samskivert-*.jar \
     $GWT_UTILS_HOME/target/classes \
     $GWT_UTILS_HOME/src/main/java \
-    environ/src/main/resources \
-    environ/src/main/java | sed 's/ /:/g'`
+    src/main/resources \
+    src/main/java | sed 's/ /:/g'`
 
 java -classpath $CLASSPATH -Xmx256M \
     com.google.gwt.dev.DevMode \
     -noserver -port 8080 -startupUrl /coreen/index.html \
-    -war environ/target/scala_2.8.0/war coreen
+    -war target/scala_2.8.0/war coreen
--- project/build/Coreen.scala
+++ project/build/Coreen.scala
@@ -1,56 +1,49 @@
-import java.net.URL
 import sbt._
 
-class Coreen (info :ProjectInfo) extends ParentProject(info) {
+class Coreen (info :ProjectInfo) extends DefaultProject(info) {
   // this project only exists to download gwt-dev, but keep it out of our normal classpaths
   lazy val gdevmode = project("gdevmode", "GWT Dev Mode", new DefaultProject(_) {
     val gwtDev = "com.google.gwt" % "gwt-dev" % "2.0.4"
   })
 
-  lazy val environ = project("environ", "Environment", new DefaultProject(_) {
-    val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
-    val scalaj_collection = "org.scalaj" %% "scalaj-collection" % "1.0"
+  // need our local repository for gwt-utils snapshot
+  val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
 
-    // need our local repository for gwt-utils snapshot
-    val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
+  // general depends
+  val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
+  val scalaj_collection = "org.scalaj" %% "scalaj-collection" % "1.0"
 
-    // HTTP and GWT depends
-    val gwtUser = "com.google.gwt" % "gwt-user" % "2.0.4"
-    val gwtUtils = "com.threerings" % "gwt-utils" % "1.1-SNAPSHOT"
-    val jetty = "org.mortbay.jetty" % "jetty" % "6.1.25"
+  // HTTP and GWT depends
+  val gwtUser = "com.google.gwt" % "gwt-user" % "2.0.4"
+  val gwtUtils = "com.threerings" % "gwt-utils" % "1.1-SNAPSHOT"
+  val jetty = "org.mortbay.jetty" % "jetty" % "6.1.25"
 
-    // database depends
-    val h2db = "com.h2database" % "h2" % "1.2.142"
-    val squeryl = "org.squeryl" % "squeryl_2.8.0" % "0.9.4-RC1"
+  // database depends
+  val h2db = "com.h2database" % "h2" % "1.2.142"
+  val squeryl = "org.squeryl" % "squeryl_2.8.0" % "0.9.4-RC1"
 
-    // generates FooServiceAsync classes from FooService classes for GWT RPC
-    val gwtAsyncGen = "com.samskivert" % "gwt-asyncgen" % "1.0"
-    lazy val genasync = runTask(Some("com.samskivert.asyncgen.AsyncGenTool"), compileClasspath,
-                                (mainJavaSourcePath ** "*Service.java" getPaths).toList)
+  // generates FooServiceAsync classes from FooService classes for GWT RPC
+  val gwtAsyncGen = "com.samskivert" % "gwt-asyncgen" % "1.0"
+  lazy val genasync = runTask(Some("com.samskivert.asyncgen.AsyncGenTool"), compileClasspath,
+                              (mainJavaSourcePath ** "*Service.java" getPaths).toList)
 
-    // generates FooMessages.java from FooMessages.properties for GWT i18n
-    lazy val i18nsync = runTask(Some("com.threerings.gwt.tools.I18nSync"), compileClasspath,
-                                mainJavaSourcePath.absolutePath :: (
-                                  mainJavaSourcePath ** "*Messages.properties" getPaths).toList)
+  // generates FooMessages.java from FooMessages.properties for GWT i18n
+  lazy val i18nsync = runTask(Some("com.threerings.gwt.tools.I18nSync"), compileClasspath,
+                              mainJavaSourcePath.absolutePath :: (
+                                mainJavaSourcePath ** "*Messages.properties" getPaths).toList)
 
-    // run our generators every time we compile
-    override def compileAction = super.compileAction dependsOn(i18nsync) dependsOn(genasync)
+  // run our generators every time we compile
+  override def compileAction = super.compileAction dependsOn(i18nsync) dependsOn(genasync)
 
-    // to cooperate nicely with GWT devmode when we run the server from within SBT, we copy (not
-    // sync) all of our resources to a target/../war directory and remove target/../resources to
-    // avoid seeing everything twice
-    def warResourcesOutputPath = outputPath / "war"
-    def copyWarResourcesAction = copyTask(mainResources, warResourcesOutputPath)
-    override def runClasspath =
-      super.runClasspath --- testResourcesOutputPath +++ warResourcesOutputPath
-    override protected def runAction = task { args =>
-      runTask(getMainClass(true), runClasspath, args) dependsOn(
-        compile, copyResources) dependsOn(copyWarResourcesAction)
-    }
-  }, javaReader)
-
-  lazy val javaReader = project("java-reader", "Java Reader", new DefaultProject(_) {
-    val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
-    val scalaj_collection = "org.scalaj" %% "scalaj-collection" % "1.0"
-  })
+  // to cooperate nicely with GWT devmode when we run the server from within SBT, we copy (not
+  // sync) all of our resources to a target/../war directory and remove target/../resources to
+  // avoid seeing everything twice
+  def warResourcesOutputPath = outputPath / "war"
+  def copyWarResourcesAction = copyTask(mainResources, warResourcesOutputPath)
+  override def runClasspath =
+    super.runClasspath --- testResourcesOutputPath +++ warResourcesOutputPath
+  override protected def runAction = task { args =>
+    runTask(getMainClass(true), runClasspath, args) dependsOn(
+      compile, copyResources) dependsOn(copyWarResourcesAction)
+  }
 }
