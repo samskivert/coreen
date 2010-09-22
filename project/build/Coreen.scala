a6fc0e7b1dc66a681553e6d09a516770a9035663
Michael Bayne <mdb@samskivert.com>
2010-09-21 19:02:12 -0700

    More progress on client building.

--- project/build/Coreen.scala
+++ project/build/Coreen.scala
@@ -22,12 +22,12 @@ class Coreen (info :ProjectInfo) extends DefaultProject(info) {
   val h2db = "com.h2database" % "h2" % "1.2.142"
   val squeryl = "org.squeryl" % "squeryl_2.8.0" % "0.9.4-RC1"
 
-  // used to add a specific dependency jar file to a path
-  def plusDep (name :String) = managedDependencyRootPath ** (name+"*")
+  // used to obtain the path for a specific dependency jar file
+  def depPath (name :String) = managedDependencyRootPath ** (name+"*")
 
   // generates FooServiceAsync classes from FooService classes for GWT RPC
   lazy val genasync = runTask(Some("com.samskivert.asyncgen.AsyncGenTool"),
-                              compileClasspath +++ plusDep("gwt-asyncgen"),
+                              compileClasspath +++ depPath("gwt-asyncgen"),
                               (mainJavaSourcePath ** "*Service.java" getPaths).toList)
 
   // generates FooMessages.java from FooMessages.properties for GWT i18n
@@ -38,12 +38,13 @@ class Coreen (info :ProjectInfo) extends DefaultProject(info) {
   // compiles our GWT client
   lazy val gwtc = runTask(
     Some("com.google.gwt.dev.Compiler"),
-    compileClasspath +++ plusDep("gwt-dev") +++ mainJavaSourcePath +++ mainResourcesPath,
+    compileClasspath +++ depPath("gwt-dev") +++ mainJavaSourcePath +++ mainResourcesPath,
     List("-war", "target/scala_2.8.0/gwtc", "coreen")) dependsOn(copyResources)
 
   // packages the output of our GWT client into a jar file
+  def packageGwtJar = outputPath / "coreen-gwt.jar"
   lazy val gwtjar = packageTask(mainResources +++ (outputPath / "gwtc" ##) ** "*",
-                                defaultJarPath("-gwt.jar"), Nil) dependsOn(gwtc)
+                                packageGwtJar, Nil) dependsOn(gwtc)
 
   // we include our resources in the -gwt.jar so we exclude them from the main jar
   override def packagePaths = super.packagePaths --- mainResources
@@ -64,9 +65,30 @@ class Coreen (info :ProjectInfo) extends DefaultProject(info) {
   }
 
   // copies the necessary files into place for our Getdown client
-  def getdownOutputPath = outputPath / "getdown"
-  lazy val getdown = task {
-    FileUtilities.copyFlat(managedClasspath(Configurations.Compile).get,
-                           getdownOutputPath, log).left.toOption
-  }
+  def clientPath = ("client" :Path)
+  def clientCodePath = clientPath / "code"
+  lazy val client = task {
+    // clean out any previous code bits
+    FileUtilities.clean(clientCodePath, log)
+
+    // copy all of the appropriate jars into the target directory
+    val clientJars = managedClasspath(Configurations.Compile) --- depPath("gwt-user") ---
+      depPath("gwt-utils")
+    FileUtilities.copyFlat(clientJars.get, clientCodePath, log)
+    FileUtilities.copyFlat(jarPath.get, clientCodePath, log)
+    FileUtilities.copyFlat(packageGwtJar.get, clientCodePath, log)
+
+    // now sanitize their names, the version numbers will just get in the way of patching
+    (clientCodePath ** "*.jar").get foreach { f =>
+      val jar = f.asFile
+      val name = jar.getName.
+        replaceAll("""_2.\d[^-]*-""", "-"). // strip _2.n.xx-xx Scala versions
+        replaceAll("""-\d\.\d.*\.jar""", ".jar"); // strip Maven and other versions
+      jar.renameTo(new java.io.File(jar.getParentFile, name))
+    }
+
+    // TODO: generate the digest file
+
+    Some("Success.")
+  } // dependsOn(packageAction) dependsOn(gwtjar)
 }
