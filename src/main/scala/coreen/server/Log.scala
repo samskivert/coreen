b2f309d8798db55aaef2b4b346b662207bb3f23e
Michael Bayne <mdb@samskivert.com>
2010-10-19 11:15:40 -0700

    Rewrote logging services to avoid using samskivert. Mainly this is because log(String message, Object... args) has type log(m :String, args :AnyRef*) in Scala which prevents us from passing AnyVals as args without extra jockeying. We really want log(m :String, args :Any*) which enables the necessary autoboxing.

--- src/main/scala/coreen/server/Log.scala
+++ src/main/scala/coreen/server/Log.scala
@@ -3,10 +3,32 @@
 
 package coreen.server
 
-import com.samskivert.util.Logger
-
 /** Provides logging services. */
 trait Log {
+  /** An interface for logging. */
+  trait Logger {
+    /** Logs a message at the debug level.
+     * @param args a series of key/value pairs and an optional final exception. */
+    def debug (msg :Any, args :Any*)
+
+    /** Logs a message at the info level.
+     * @param args a series of key/value pairs and an optional final exception. */
+    def info (msg :Any, args :Any*)
+
+    /** Logs a message at the warning level.
+     * @param args a series of key/value pairs and an optional final exception. */
+    def warning (msg :Any, args :Any*)
+
+    /** Logs a message at the error level.
+     * @param args a series of key/value pairs and an optional final exception. */
+    def error (msg :Any, args :Any*)
+
+    /** Formats a debug message.
+     * @param args key/value pairs, (e.g. "age", someAge, "size", someSize) which will be appended
+     * to the log message as [age=someAge, size=someSize]. */
+    def format (message :Any, args :Any*) :String
+  }
+
   /** For great logging. */
   val _log :Logger
 }
@@ -14,5 +36,49 @@ trait Log {
 /** A concrete implementation of {@link Log}. */
 trait LogComponent extends Component with Log {
   /** For great logging. */
-  val _log = com.samskivert.util.Logger.getLogger("coreen")
+  val _log = new Logger {
+    def debug (msg :Any, args :Any*) = doLog(0, format(msg, args :_*), getError(args :_*))
+    def info (msg :Any, args :Any*) = doLog(1, format(msg, args :_*), getError(args :_*))
+    def warning (msg :Any, args :Any*) = doLog(2, format(msg, args :_*), getError(args :_*))
+    def error (msg :Any, args :Any*) = doLog(3, format(msg, args :_*), getError(args :_*))
+
+    def format (message :Any, args :Any*) = {
+      val sb = new StringBuilder().append(message)
+      if (!args.isEmpty) sb.append(" [").append(formatArgs(args)).append("]")
+      sb.toString
+    }
+
+    protected def formatArgs (args :Seq[Any]) = args.grouped(2).map(_ match {
+      case Seq(k, v) => Some(k + "=" + safeToString(v))
+      case Seq(a) => a match {
+        case ex :Throwable => None
+        case arg => Some(arg + "=<odd arg>")
+      }
+    }).toList.flatten.mkString(", ")
+
+    protected def safeToString (arg :Any) =
+      try { String.valueOf(arg) }
+      catch { case t => "<toString() failure: " + t + ">" }
+
+    protected def getError (args :Any*) =
+      if (args.length % 2 == 0) None else args.last match {
+        case exn :Throwable => Some(exn)
+        case _ => None
+      }
+
+    protected def doLog (level :Int, fmsg :String, error :Option[Throwable]) {
+      val sb = new StringBuffer
+      _date.setTime(System.currentTimeMillis)
+      _format.format(_date, sb, _fpos)
+      sb.append(" ").append(LevelNames(level)).append(" ").append(fmsg)
+      System.err.println(sb)
+      error foreach { _.printStackTrace(System.err) }
+    }
+
+    private val _date = new java.util.Date
+    private val _format = new java.text.SimpleDateFormat("yyyy/MM/dd HH:mm:ss:SSS")
+    private val _fpos = new java.text.FieldPosition(java.text.DateFormat.DATE_FIELD)
+
+    private val LevelNames = Array("DEBUG", "INFO", "WARN", "ERROR")
+  }
 }
